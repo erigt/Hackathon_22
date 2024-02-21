ed282a9343b05f3c1e72218d094f44a6de1a87fb
ImLauraGS <lgilsolano@gmail.com>
2024-02-21 21:12:53 +0100

    estructura html details

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,7 +1,23 @@
 
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import styles from './details.module.css'
 
 export default function Details() {
   return (
-    <div>Details</div>
+    <>
+      <Header text="Iniciar Sesion"/>
+        <main>
+          <article className={styles.detailsContainer}>
+            <img src="/dog.webp" alt="" />
+            <h3>¡Paseo a tu perro!</h3>
+            <h4>10€/hora</h4>
+            <h4>name@mail.com</h4>
+            <h4>Servicios</h4>
+            <p>¿Necesitas un poco de ayuda para asegurarte de que tu mejor amigo peludo obtenga la dosis adecuada de ejercicio y diversión? ¡No busques más! Soy un amante de los perros con años de experiencia en el cuidado y la atención de mascotas.</p>
+          </article>
+        </main>
+      <Footer/>
+    </>
   )
 }
