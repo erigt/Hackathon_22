36f69556927382dfaecc44088cb85ea0e6d19234
Erika <146768635+erigt@users.noreply.github.com>
2024-02-21 17:40:02 +0100

    datasql

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO announcements (id, title, description, contact, price, image, category, location) VALUES (default, '', 'descripcion', 'contacto', 'precio', 'imagen', 'category', 'localizacion');
