9c0ab976037251da288e1b3529da1312306bb455
ImLauraGS <lgilsolano@gmail.com>
2024-02-22 13:02:30 +0100

    estilos page details hecho

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import styles from './details.module.css'
+import Btn from "../components/Button/Btn";
 
 
 export default function Details() {
@@ -16,18 +18,24 @@ export default function Details() {
     animalDetail();
   }, [id]);
   return (
-    <div>
+    <>
       <Header text={'Cerrar Sesion'} />
-      <img
-        src={animal.imageUrl}
-        alt={animal.title} />
-      <h3>{animal.title}</h3>
-      <p>{animal.price}</p>
-      <p>{animal.contact}</p>
-      <p>{animal.location}</p>
-      <p>{animal.category}</p>
-      <p>{animal.description}</p>
-      <Footer />
-    </div>
+      <main className={styles.detailsContainer}>
+        <article>
+          <img
+            src={animal.imageUrl}
+            alt={animal.title} />
+          <h3>{animal.title}</h3>
+          <div>
+            <p>{animal.price}€</p>
+            <p>{animal.contact}</p>
+            <p>{animal.location}</p>
+          </div>
+          <p>{animal.description}</p>
+        </article>
+        <Btn color='tertiary' text='Contacta'/>
+      </main>
+    <Footer />
+    </>
   )
 }
