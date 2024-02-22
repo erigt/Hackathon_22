33a28eef94a88a14f52632efc7d1eaadffb36ebe
mava <mavafiza@gmail.com>
2024-02-22 14:01:25 +0100

    Creando boton Hazte Vip

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,17 +4,18 @@ import Btn from '../Button/Btn';
 import styles from './header.module.css'
 import { Link, NavLink } from 'react-router-dom';
 
-export default function Header({ text }) {
+export default function Header({ text1, text2 }) {
   return (
     <header className={styles.headerContainer}>
       <Link to='/'><img src={Logo} alt="" /></Link>
       <div className={styles.btnGroup}>
-        <NavLink to="/addform"><Btn color='primary' text={text}></Btn></NavLink>
+        <NavLink to="/vip"><Btn color='secondary' text={text2}></Btn></NavLink>
+        <NavLink to="/addform"><Btn color='primary' text={text1}></Btn></NavLink>
       </div>
     </header>
   )
 }
 
 Header.propTypes = {
-  text: PropTypes.string.isRequired,
+  text1: PropTypes.string.isRequired, text2: PropTypes.string.isRequired, 
 };
